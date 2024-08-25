ea88b82feb0c5c9df3f45901dbca1a6057baa1df
Sehastrajit <sehastrajit@gmail.com>
2024-08-25 11:20:30 +0530

    s

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='face_detection_lib',
-    version='0.6',  # Increment the version number
+    name='face_access_detection',  # Update with your library name
+    version='0.1',  # Increment the version number
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
@@ -12,9 +12,9 @@ setup(
         'numpy',
     ],
     package_data={
-        'face_detection_lib': ['models/best.pt'],  # Ensure this path is correct
+        'face_access_detection': ['models/best.pt'],  # Ensure this path is correct
     },
-    description='A library for face detection using YOLOv8',
+    description='A library for face access detection using YOLOv8',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     url='https://github.com/Sehastrajit/face-access-detection',  # Update with your GitHub repo URL
